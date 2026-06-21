06d73b020624be2e5015493a6c2394ee4948377e
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-21 11:58:58 -0300

    feat(api): add `forbiddenError` constant

--- apps/api/src/utils/errors.js
+++ apps/api/src/utils/errors.js
@@ -1,7 +1,7 @@
 import { STATUS_CODES } from 'node:http';
 import { constants } from 'node:http2';
 
-const { HTTP_STATUS_INTERNAL_SERVER_ERROR } = constants;
+const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_FORBIDDEN } = constants;
 
 export class HttpError extends Error {
 	constructor(status = HTTP_STATUS_INTERNAL_SERVER_ERROR, message = STATUS_CODES[status]) {
@@ -9,5 +9,7 @@ export class HttpError extends Error {
 		this.status = status;
 	}
 }
+
+export const forbiddenError = new HttpError(HTTP_STATUS_FORBIDDEN);
 
 export const isHttpError = (error) => error instanceof HttpError;
