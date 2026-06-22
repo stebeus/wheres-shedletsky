bc3437e390f8e1bd68aba2f226c963c244e753c4
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-22 11:54:07 -0300

    fix(api): import `sql` and split position

--- apps/api/src/modules/characters/services.js
+++ apps/api/src/modules/characters/services.js
@@ -1,12 +1,21 @@
+import { sql } from 'drizzle-orm';
+
 import { characters } from '#root/db/schema.js';
 import { db } from '#root/lib/drizzle.js';
 
 export const findCharacters = async () => await db.select().from(characters);
 
-export const isCharacter = async (table, condition) => {
-	const [{ exists }] = await db.execute(
-		sql`SELECT EXISTS (SELECT 1 FROM ${characters} WHERE name = ${name} AND position ~= POINT(${position}))`,
-	);
+export const isCharacter = async ({ name, position: [x, y] }) => {
+	const query = sql`
+		SELECT EXISTS (
+			SELECT 1
+			FROM ${characters}
+			WHERE ${characters.name} = ${name}
+				AND ${characters.position} ~= point(${x}, ${y})
+		)
+	`;
+
+	const [{ exists }] = await db.execute(query);
 
 	return exists;
 };
