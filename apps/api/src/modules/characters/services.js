ff95e3cca5650c5f32be574d0d5e75746573c388
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-22 10:24:23 -0300

    refactor(api)!: move exists query to the service itself to prevent SQL injection

--- apps/api/src/modules/characters/services.js
+++ apps/api/src/modules/characters/services.js
@@ -1,8 +1,12 @@
 import { characters } from '#root/db/schema.js';
 import { db } from '#root/lib/drizzle.js';
-import { selectExists } from '#root/utils/queries.js';
 
 export const findCharacters = async () => await db.select().from(characters);
 
-export const isCharacter = async ({ name, position }) =>
-	await selectExists(characters, `name = ${name} AND position ~= POINT(${position})`);
+export const isCharacter = async (table, condition) => {
+	const [{ exists }] = await db.execute(
+		sql`SELECT EXISTS (SELECT 1 FROM ${characters} WHERE name = ${name} AND position ~= POINT(${position}))`,
+	);
+
+	return exists;
+};
