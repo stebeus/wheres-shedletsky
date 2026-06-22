902c795930a21f25cf50f0a3ffa9f14946bb795c
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-22 09:44:41 -0300

    refactor(api)!: improve `characters` structure

--- apps/api/src/modules/characters/handlers.js
+++ apps/api/src/modules/characters/handlers.js
@@ -1,8 +1,11 @@
+import validate from 'express-zod-safe';
+
 import { findCharacters, isCharacter } from './services.js';
+import { query } from './validations.js';
 
 export const getMany = async (req, res, next) => res.send({ data: await findCharacters() });
 
-export const getIsCharacter = async (req, res, next) => {
-	const { name, position } = req.query;
-	res.send({ isCharacter: await isCharacter(position, name) });
-};
+export const getIsCharacter = [
+	validate({ query }),
+	async ({ query }, res, next) => res.send({ isCharacter: await isCharacter(query) }),
+];
--- apps/api/src/modules/characters/services.js
+++ apps/api/src/modules/characters/services.js
@@ -1,14 +1,8 @@
-import { sql } from 'drizzle-orm';
-
 import { characters } from '#root/db/schema.js';
 import { db } from '#root/lib/drizzle.js';
+import { selectExists } from '#root/utils/queries.js';
 
 export const findCharacters = async () => await db.select().from(characters);
 
-export const isCharacter = async (position, name) => {
-	const [x, y] = position.split('_');
-
-	return await db.execute(
-		sql`SELECT EXISTS (SELECT 1 FROM characters WHERE position ~= point(${x}, ${y}) AND name = ${name})`,
-	);
-};
+export const isCharacter = async ({ name, position }) =>
+	await selectExists(characters, `name = ${name} AND position ~= POINT(${position})`);
